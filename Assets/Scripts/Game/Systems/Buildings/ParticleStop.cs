3566fe0fa63529357844038aaa20291e46a95393
brizbird14 <brizbird14@gmail.com>
2022-04-18 12:42:07 -0400

    Adjust particle size
    Particles generated when placing building diff sizes depending on building size

--- Assets/Scripts/Game/Systems/Buildings/ParticleStop.cs
+++ Assets/Scripts/Game/Systems/Buildings/ParticleStop.cs
@@ -6,17 +6,16 @@ public class ParticleStop : MonoBehaviour
 {
     // Start is called before the first frame update
     ParticleSystem particles;
+    //[SerializeField] float size;
     private IEnumerator coroutineDestroy;
     void Awake()
     {
         this.gameObject.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
+        //this.gameObject.transform.localScale = new Vector3(size, size, size);
+
         // Destroys particle system after 2 seconds
         coroutineDestroy = destroyParticles(particles);
         StartCoroutine(coroutineDestroy);
-    }
-
-    public void scaleParticles() {
-
     }
 
     private IEnumerator destroyParticles(ParticleSystem sys) {
